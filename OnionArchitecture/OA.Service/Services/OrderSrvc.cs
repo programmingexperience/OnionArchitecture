ba5ba6c4a22852b0993525f7f098f260cbb0210c
SAGAR\sagar-parida <programming.experience@gmail.com>
2017-06-07 19:29:16 +0530

    Commit OA.Data/OA.Repo/OA.Service Layer

---
+++ OnionArchitecture/OA.Service/Services/OrderSrvc.cs
@@ -0,0 +1,24 @@
+﻿using OA.Data.Model;
+using OA.Repo.UoW;
+using OA.Service.Interfaces;
+
+namespace OA.Service.Services
+{
+    public class OrderSrvc : IOrderSrvc
+    {
+        private readonly IGenericUoW _UoW2;
+        public OrderSrvc(IGenericUoW UoW2)
+        {
+            _UoW2 = UoW2;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="order"></param>
+        /// <returns></returns>
+        public void InsertOrder(Order order)
+        {
+            _UoW2.GenericRepository<Order>().Insert(order);
+        }
+    }
+}
---
+++ OnionArchitecture/OA.Service/Services/UserSrvc.cs
@@ -0,0 +1,64 @@
+﻿using OA.Data.Model;
+using OA.Repo.UoW;
+using OA.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OA.Service.Services
+{
+    public class UserSrvc : IUserSrvc
+    {
+        private readonly IGenericUoW _UoW2;
+        public UserSrvc(IGenericUoW UoW2)
+        {
+            _UoW2 = UoW2;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Username"></param>
+        /// <param name="PasswordHash"></param>
+        /// <returns></returns>
+        public User ValidateUser(string Username, string PasswordHash)
+        {
+            return _UoW2.GenericRepository<User>().GetFirstOrDefault(x => x.Email == Username && x.PasswordHash == PasswordHash);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<User> Get()
+        {
+            return _UoW2.GenericRepository<User>().Get().ToList();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        public Int64 InsertUserOrder(User user, Order order)
+        {
+            Int64 uid = 0;
+            using (var _dbtransaction = _UoW2.BeginTransaction())
+            {
+                try
+                {
+                    _UoW2.GenericRepository<User>().Insert(user);
+                    _UoW2.SaveChanges();
+                    uid = user.Id;
+                    order.UserId = uid;
+
+                    _UoW2.GenericRepository<Order>().Insert(order);
+                    _UoW2.SaveChanges();
+                    _dbtransaction.Commit();
+                }
+                catch (Exception)
+                {
+                    _dbtransaction.Rollback();
+                }
+                return uid;
+            }
+        }
+    }
+}
